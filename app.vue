ded2b8c2c092a8afb83da143d15e463a58accfd4
Yaroslav Nozdrin <154721635+yazorinv@users.noreply.github.com>
2024-04-24 22:53:53 +0500

    Add backgrounds for themes

--- app.vue
+++ app.vue
@@ -4,4 +4,16 @@
 
 <style lang="scss">
 @import "/styles/base-colors.scss";
+@import "/styles/colors.scss";
+
+
+body {
+  background-color: $crust;
+  padding-left: 10%;
+  padding-right: 10%;
+}
+
+.dark-mode body {
+  background: $crust url("/background-dark.png") no-repeat;
+}
 </style>
